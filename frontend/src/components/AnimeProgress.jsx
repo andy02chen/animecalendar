ab01eacc89682727973c0432af1dbacfa4c86a00
Andy Chen <andy02chen@gmail.com>
2024-06-12 21:03:12 +1000

    added placeholders for weekly watching list

--- frontend/src/components/AnimeProgress.jsx
+++ frontend/src/components/AnimeProgress.jsx
@@ -1,8 +1,19 @@
+import './AnimeProgress.css'
 
 function AnimeProgress() {
     return(
         <>
-            <p>Weekly Anime Progress</p>
+            <div className='heading'>
+                <h1 className='title'>Weekly Anime Progress</h1>
+            </div>
+
+            <div className='progress-div'>
+                <uL className='anime-list'>
+                    <li className='weekly-anime'>Anime</li>
+                    <li className='weekly-anime'>Anime</li>
+                    <li className='weekly-anime'>Anime</li>
+                </uL>
+            </div>
         </>
     );
 }
--- frontend/src/components/MainComponent.jsx
+++ frontend/src/components/MainComponent.jsx
@@ -10,11 +10,11 @@ function MainComponent() {
                 <div className="anime-progress-div">
                     <AnimeProgress/>
                 </div>
-                
+
                 <div className="calendar-div">
                     <div className="settings-div">
                         <div className="left">
-                            <h1>Month</h1>
+                            <h1 className="title">Month</h1>
                             <button>Prev Month</button>
                             <button>Next Month</button>
                         </div>
