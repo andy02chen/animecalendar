df01c583f770375e60bba8cfb07d08bc68bf4364
Andy Chen <andy02chen@gmail.com>
2024-06-21 17:20:42 +1000

    feature: user is able to skip rating fix: change displayed div after rating

--- frontend/src/components/RateAnime.jsx
+++ frontend/src/components/RateAnime.jsx
@@ -1,17 +1,27 @@
 import axios from "axios";
 
 // Updates the number of episodes watched on MyAnimeList
-function updateStatus(anime, setRefreshAnimeDisplay, score) {
-    axios.post('/api/update-anime',
-        {
+function updateStatus(anime, setRefreshAnimeDisplay, score, displayDiv) {
+    let data = {}
+
+    if(score === 0) {
+        data = {
+            'anime-id': anime.id,
+            'eps-watched': anime.eps_watched
+        }
+    } else {
+        data = {
             'anime-id': anime.id,
             'eps-watched': anime.eps_watched,
             'score': score
         }
-    )
+    }
+
+    axios.post('/api/update-anime', data)
     .then(response => {
         anime.eps_watched++;
         setRefreshAnimeDisplay(prevFlag => !prevFlag);
+        displayDiv('rating',anime.id);
     })
     .catch(error => {
         // TODO Display error
@@ -24,17 +34,17 @@ function RateAnime({anime, setRefreshAnimeDisplay, displayDiv}) {
     return(
         <div className={'rating'+anime.id} style={{display:'none'}}>
             <h3>Would you like to leave a rating?</h3>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 1)}>1</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 2)}>2</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 3)}>3</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 4)}>4</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 5)}>5</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 6)}>6</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 7)}>7</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 8)}>8</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 9)}>9</button>
-            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 10)}>10</button>
-            <button>Skip</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 1, displayDiv)}>1</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 2, displayDiv)}>2</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 3, displayDiv)}>3</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 4, displayDiv)}>4</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 5, displayDiv)}>5</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 6, displayDiv)}>6</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 7, displayDiv)}>7</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 8, displayDiv)}>8</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 9, displayDiv)}>9</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 10, displayDiv)}>10</button>
+            <button onClick={() => updateStatus(anime, setRefreshAnimeDisplay, 0, displayDiv)}>Skip</button>
         </div>
     );
 }
