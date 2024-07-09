7115d8002c6deea68fc28ed9cf57cc6defd7113d
Andy Chen <andy02chen@gmail.com>
2024-07-09 21:29:14 +1000

    Changes to settings div
    - Settings div will disappear when clicked off

--- frontend/src/components/AnimeProgress.jsx
+++ frontend/src/components/AnimeProgress.jsx
@@ -134,6 +134,16 @@ function AnimeProgress() {
         getWeeklyAnime(setWeeklyAnime,setDisplayAnime,setFailedRequest,setGotRequest);
     },[]);
 
+    document.addEventListener("click", function(event) {
+        const div = document.getElementById('settings-options-div');
+        const username = document.getElementById('settings-div-show-btn')
+        let isClickInside = div.contains(event.target) || username.contains(event.target);
+    
+        if (!isClickInside) {
+            div.style.display = "none";
+        }
+    });
+
     return(
         <>
             <div className='heading'>
@@ -144,7 +154,7 @@ function AnimeProgress() {
                         PROGRESS</h1>
 
                     <div className='settings-dropdown'>
-                        <button className='dropdown-btn' onClick={() => displaySettings()}>
+                        <button className='dropdown-btn' id='settings-div-show-btn' onClick={() => displaySettings()}>
                             Username
                             <svg className='settings-div-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
                         </button>
