dbc06d78f3f6ec89e4b0a6c865a025fb5d57137f
Andy Chen <andy02chen@gmail.com>
2024-07-11 16:57:53 +1000

    added section for currently watching anime

--- frontend/src/components/Anime/AnimeProgress.jsx
+++ frontend/src/components/Anime/AnimeProgress.jsx
@@ -43,34 +43,38 @@ const renderContent = (weeklyAnime, displayAnime, failedRequest, setWeeklyAnime,
         // Displays list of anime
         if(displayAnime.length > 0) {
             return(
-                <ul className='anime-list'>
-                    {displayAnime.map((anime,index) =>
-                        <li key={index} className='weekly-anime'>
-                            <div className='anime'>
+                <>
+                    <div className='progress-section-div'>
+                        <p className='progress-section-heading'>Watching</p>
+                    </div>
+                    <ul className='anime-list'>
+                        {displayAnime.map((anime,index) =>
+                            <li key={index} className='weekly-anime'>
+                                <div className='anime'>
+                                    <div className='anime-top-div'>
+                                        <h1 className='anime-list-title'>{anime.title}</h1>
+                                    </div>
+                                    <div className='anime-bot-div'>
+                                        <div>
+                                            <img className='weekly-anime-img' src={anime.img} alt={`Image of ${anime.title}`}></img>
+                                        </div>
 
-                                <div className='anime-top-div'>
-                                    <h1 className='anime-list-title'>{anime.title}</h1>
+                                        <div id={anime.id+'ep-details-div'} className='anime-bot-ep'>
+                                            <AnimeAvailableDate anime={anime}/>
+                                        </div>
+
+                                        <div id={anime.id+'update-spinner'} className='update-div'>
+                                            <svg className="update-spinner" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path  d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8C121.8 95.6 64 169.1 64 256c0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1c-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256c0 141.4-114.6 256-256 256S0 397.4 0 256C0 140 77.1 42.1 182.9 10.6c16.9-5 34.8 4.6 39.8 21.5z"/></svg>
+                                            <p>Updating...</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className='anime-bot-div'>
-                                    <div>
-                                        <img className='weekly-anime-img' src={anime.img} alt={`Image of ${anime.title}`}></img>
-                                    </div>
-
-                                    <div id={anime.id+'ep-details-div'} className='anime-bot-ep'>
-                                        <AnimeAvailableDate anime={anime}/>
-                                    </div>
-
-                                    <div id={anime.id+'update-spinner'} className='update-div'>
-                                        <svg className="update-spinner" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                        <path  d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8C121.8 95.6 64 169.1 64 256c0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1c-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256c0 141.4-114.6 256-256 256S0 397.4 0 256C0 140 77.1 42.1 182.9 10.6c16.9-5 34.8 4.6 39.8 21.5z"/></svg>
-                                        <p>Updating...</p>
-                                    </div>
-                                </div>
-                            </div>
-                            {index === displayAnime.length - 1 ? <div></div> : <div className='anime-div-bar'></div> }
-                        </li>
-                    )}
-                </ul>
+                                {index === displayAnime.length - 1 ? <div></div> : <div className='anime-div-bar'></div> }
+                            </li>
+                        )}
+                    </ul>
+                </>
             )
         } else if(weeklyAnime.length == 0) {
             return(
