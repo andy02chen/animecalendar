80749bd9487f5943f426a77906f900bdfc0f5da3
Andy Chen <andy02chen@gmail.com>
2024-07-13 10:53:24 +1000

    Added functionality for checkbox
    able to filter for only not yet aired anime

--- frontend/src/components/Anime/AnimeProgress.jsx
+++ frontend/src/components/Anime/AnimeProgress.jsx
@@ -65,8 +65,17 @@ const displayCurrAiring = (event, currAiringAnime,weeklyAnime, setDisplayAnime) 
     }
 }
 
+// Displays Not Yet Aired Anime
+const displayNotYetAired = (event, notYetAiredList, planToWatchAnimeList, setPlanToWatch) => {
+    if(event.target.checked) {
+        setPlanToWatch(notYetAiredList);
+    } else {
+        setPlanToWatch(planToWatchAnimeList);
+    }
+}
+
 // Displays weekly anime
-const renderContent = (notYetAiredList,planToWatchAnimeList,displayPlanToWatch,currAiringAnime, weeklyAnime, displayAnime, failedRequest, setWeeklyAnime, setDisplayAnime, setFailedRequest, setGotRequest) => {
+const renderContent = (notYetAiredList,planToWatchAnimeList,displayPlanToWatch,currAiringAnime, weeklyAnime, displayAnime, failedRequest,setPlanToWatch, setWeeklyAnime, setDisplayAnime, setFailedRequest, setGotRequest) => {
     // Returns error message if failed get request
     if(failedRequest) {
         const popup = document.getElementById("error-popup");
@@ -126,7 +135,7 @@ const renderContent = (notYetAiredList,planToWatchAnimeList,displayPlanToWatch,c
                         <>
                             <div id='plan-to-watch-animes-div' className='progress-section-div plan-to-watch-div' onClick={() => expandPlanToWatchDiv()}>
                                 <p className='progress-section-heading'>Plan To Watch</p>
-                                <input onClick={(event) => event.stopPropagation()}  type='checkbox' id='plan-to-watch-check' name='plan-to-watch-check' value="plan-to-watch-value"></input>
+                                <input onChange={(event) => displayNotYetAired(event, notYetAiredList, planToWatchAnimeList, setPlanToWatch)} onClick={(event) => event.stopPropagation()}  type='checkbox' id='plan-to-watch-check' name='plan-to-watch-check' value="plan-to-watch-value"></input>
                                 <label onClick={(event) => event.stopPropagation()} htmlFor="plan-to-watch-check" className='anime-progress-checkbox'> Show Not Yet Aired Only</label>
                             </div>
                             <div id='anime-list-div-plan-to-watch' style={{display: 'none'}}>
@@ -318,7 +327,7 @@ function AnimeProgress() {
             </div>
             <div className='progress-div'>
                     {gotRequest ?
-                        renderContent(notYetAiredList,planToWatchAnimeList,displayPlanToWatch,currAiringAnime, weeklyAnime, displayAnime, failedRequest, setWeeklyAnime, setDisplayAnime, setFailedRequest, setGotRequest)
+                        renderContent(notYetAiredList,planToWatchAnimeList,displayPlanToWatch,currAiringAnime, weeklyAnime, displayAnime, failedRequest,setPlanToWatch, setWeeklyAnime, setDisplayAnime, setFailedRequest, setGotRequest)
                         :
                         <div className='message-div'>
                             <svg className='loading-spinner' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8C121.8 95.6 64 169.1 64 256c0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1c-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256c0 141.4-114.6 256-256 256S0 397.4 0 256C0 140 77.1 42.1 182.9 10.6c16.9-5 34.8 4.6 39.8 21.5z"/></svg>
