e44384c86a4fcc48f4e9a0dc832c5dd47abd0b6c
Andy Chen <andy02chen@gmail.com>
2024-07-26 23:53:42 +1000

    UI updates
    - Added dates for prev, curr and next month

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,89 +1,115 @@
 import './Calendar.css'
+import './Month.css';
 
 function Calendar() {
     const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    // Today's Date
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+
+    // Get start dates
+    const startDate = new Date(year, month, 1).getDay();
+    const endDate = new Date(year, month + 1, 0).getDate();
+
+    // Get Prev Month End
+    const prevMonthStart = new Date(year, month, 0).getDay();
+    let prevMonthDate = new Date(year, month, 0).getDate();
+
+    // Get Next Month Start
+    let nextMonthStart = new Date(year, month + 1, 1).getDate();
+    const nextMonthDay = new Date(year, month + 1, 1).getDay();
+    console.log(nextMonthStart, nextMonthDay);
+
+    // Array of prev month dates
+    const inactiveDays = [];
+    for(let i = prevMonthStart; i < startDate; i++) {
+        inactiveDays.push(prevMonthDate++);
+    }
+
+    // Array of curr month dates
+    const currMonthDates = [];
+    for(let i = startDate; i <= endDate; i++) {
+        currMonthDates.push(i);
+    }
+
+    // Array of next month dates
+    const nextMonthDates = [];
+    for(let i = nextMonthDay; i < 7; i++) {
+        nextMonthDates.push(nextMonthStart++);
+    }
+
     return(
-        <div className='calendar-main'>
-            <div>
-                <div className='days-div'>
-                    <ul className='days-of-week'>
-                        {daysOfWeek.map((day, index) => 
-                            <li key={index}>
-                                <h1 className='day-title'>{day}</h1>
-                            </li>
-                        )}
-                    </ul>
+        <>
+            <div className="month-div">
+                <div className='month-display-div'>
+                <button className="prev-month-button">
+                    <svg className="prev-month-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                </button>
+                <div>
+                    <h1 className="month-heading unselectable">{months[month]} {year}</h1>
                 </div>
-                <div className='dates-div'>
-                    <ul className='dates-list'>
-                        <li className='inactive-date'>
-                            <div>
-                                <p>28</p>
-                            </div>
-                        </li>
-                        <li className='inactive-date'>
-                            <div>
-                                <p>29</p>
-                            </div>
-                        </li>
-                        <li className='inactive-date'>
-                            <div>
-                                <p>30</p>
-                            </div>
-                        </li>
-                        <li className='inactive-date'>
-                            <div>
-                                <p>31</p>
-                            </div>
-                        </li>
-                        <li>
-                            <div>
-                                <p>1</p>
-                            </div>
-                        </li>
-                        <li>
-                            <div>
-                                <p>2</p>
-                            </div>
-                        </li>
-                        <li className='today'>
-                            <div>
-                                <p>3</p>
-                            </div>
-                        </li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>6</li>
-                        <li>7</li>
-                        <li>8</li>
-                        <li>9</li>
-                        <li>10</li>
-                        <li>11</li>
-                        <li>12</li>
-                        <li>13</li>
-                        <li>14</li>
-                        <li>15</li>
-                        <li>16</li>
-                        <li>17</li>
-                        <li>18</li>
-                        <li>19</li>
-                        <li>20</li>
-                        <li>21</li>
-                        <li>22</li>
-                        <li>23</li>
-                        <li>24</li>
-                        <li>25</li>
-                        <li>26</li>
-                        <li>27</li>
-                        <li>28</li>
-                        <li>29</li>
-                        <li>30</li>
-                        <li className='inactive-date'>1</li>
-                    </ul>
+                <button className="next-month-button">
+                    <svg className="next-month-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
+                </button>
+            </div>
+            </div>
+            <div className='calendar-main'>
+                <div>
+                    <div className='days-div'>
+                        <ul className='days-of-week'>
+                            {daysOfWeek.map((day, index) => 
+                                <li key={index}>
+                                    <h1 className='day-title'>{day}</h1>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    <div className='dates-div'>
+                        <ul className='dates-list'>
+                            {inactiveDays.map((date, index) => 
+                                <li key={index} className='inactive-date'>
+                                    <div>
+                                        <p>{date}</p>
+                                    </div>
+                                </li>
+                            )}
+                            {currMonthDates.map((date, index) => 
+                                <li key={index} className={date === day ? "today": null}>
+                                    <div>
+                                        <p>{date}</p>
+                                    </div>
+                                </li>
+                            )}
+                            {nextMonthDates.map((date, index) => 
+                                <li key={index} className='inactive-date'>
+                                    <div>
+                                        <p>{date}</p>
+                                    </div>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
