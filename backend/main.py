dbc4059cca425d8cae5996da8cec2940365cefff
Andy Chen <andy02chen@gmail.com>
2024-09-01 22:27:17 +1000

    Added tiny text to footer

--- backend/main.py
+++ backend/main.py
@@ -793,5 +793,5 @@ if __name__ == '__main__':
         # db.drop_all()
         db.create_all()
     
-    app.run(debug=True,port=5000)
-    # app.run(debug=True,port=5000, ssl_context=('localhost.pem', 'localhost-key.pem'))
+    # app.run(debug=True,port=5000)
+    app.run(debug=True,port=5000, ssl_context=('localhost.pem', 'localhost-key.pem'))
