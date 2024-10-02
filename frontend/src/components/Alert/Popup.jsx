92d7aee997fa537e6164ac4fa85a86445c8560f7
Andy Chen <andy02chen@gmail.com>
2024-10-02 16:24:58 +1000

    Updated Localstorage and popup notif

--- frontend/src/components/Alert/Popup.jsx
+++ frontend/src/components/Alert/Popup.jsx
@@ -8,7 +8,7 @@ function Popup() {
         const consentGiven = localStorage.getItem('consentGiven');
         if (!consentGiven) {
             setIsVisible(true);
-            document.getElementById('main-div').style.overflow = 'hidden';
+            // document.getElementById('main-div').style.overflow = 'hidden';
         } else {
             setIsVisible(false);
         }
@@ -27,21 +27,25 @@ function Popup() {
 
     return(
         isVisible && (
-            <div className="gray-screen">
-                <div className="popup-localStorage">
-                    <h2 className="popup-localStorage-title">This site uses Cookies and LocalStorage</h2>
-                    <p className="popup-localStorage-text">
-                        Preferences and some anime tracking data are saved in cookies and localStorage.<br/>
-                        Clearing your browser data or using incognito mode may result in the loss of this information.<br/>
-                        Your anime delayed data will not be saved across devices. (May change in a future update)
-                    </p>
-                    <div className="popup-button-div">
-                        <button onClick={() => handleAccept()}>Accept</button>
-                        <button onClick={() => handleAcceptForever()}>Accept and Hide</button>
+            <div className="localstorage-popup">
+                <div className="localstorage-popup-shape">
+                    <svg className="popup-svg-shape" preserveAspectRatio="none" viewBox="0 0 374 217" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 216V1H373V193L344 216H1Z" fill="#1A344E" stroke="#3FA4FF" stroke-width="2"/>
+                    </svg>
+                    <div className="popup-contain-text">
+                        <h1 className="popup-content-title">This site uses Cookies and LocalStorage</h1>
+                        <p className="popup-content-text">
+                            Preferences and some anime tracking data are saved in cookies and localStorage.<br/>
+                            Clearing your browser data or using incognito mode may result in the loss of this information.<br/>
+                            Your anime delayed data will not be saved across devices. (May change in a future update)
+                        </p>
+                        <div className="popup-content-buttons">
+                            <button onClick={() => handleAccept()}>Accept</button>
+                            <button onClick={() => handleAcceptForever()}>Accept and Hide</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            
         )
     );
 }
--- frontend/src/components/Pages/CalendarPage.jsx
+++ frontend/src/components/Pages/CalendarPage.jsx
@@ -2,12 +2,14 @@ import NewCalendar from '../Calendar/NewCalendar';
 import ProgressContainer from '../Progress/ProgressContainer';
 import Feedback from '../MenuPages/Feedback';
 import Tutorial from '../MenuPages/Tutorial';
+import Popup from '../Alert/Popup';
 import './CalendarPage.css';
 
 function CalendarPage() {
 
     return(
         <>
+            <Popup/>
             <div className='app-main'>
                 <ProgressContainer/>
                 <NewCalendar/>
