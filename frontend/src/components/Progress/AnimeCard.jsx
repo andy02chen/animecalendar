f4e9ebbe5a937f8730907f6dfd68dac42d42982b
Andy Chen <andy02chen@gmail.com>
2024-10-11 16:26:53 +1100

    Added loading indcator when updating progress

--- frontend/src/components/Progress/AnimeCard.jsx
+++ frontend/src/components/Progress/AnimeCard.jsx
@@ -20,7 +20,8 @@ function decreaseAnimeProgress(anime, setUpdate) {
     }
 }
 
-function updateAnimeProgress(anime, setUpdate) {
+function updateAnimeProgress(anime, setUpdate, setLoading) {
+    setLoading(true);
     anime.updateWatchedEpisodes().then((result) => {
     if (result) {
         setUpdate(u => !u);
@@ -28,12 +29,14 @@ function updateAnimeProgress(anime, setUpdate) {
         console.log('API call failed.');
         // TODO make error popup
     }
+    setLoading(false);
     });
 }
 
 function AnimeCard({anime, type}) {
 
     const [update, setUpdate] = useState(false);
+    const [loading, setLoading] = useState(false);
     
     // Progress Bar Styling
     let progress = anime.totalEpisodes === 0 ? 60: (anime.currentProgress / anime.totalEpisodes) * 100;
@@ -65,7 +68,7 @@ function AnimeCard({anime, type}) {
                     <img src={anime.image} alt='Img of Anime'/>
                 </div>
                 <div className='card-progress-div'>
-                    {type === "cw" && 
+                    {type === "cw" && !loading && 
                         <>
                             <div className='anime-card-progress-buttons'>
                                 <button className="anime-card-change-progress-button" id={`decrease-progress-${anime.id}`} disabled={anime.currentProgress <= anime.minProgress} 
@@ -79,11 +82,21 @@ function AnimeCard({anime, type}) {
                                     Delayed
                                 </button>
                                 <button className="card-progress-button positive-button" disabled={anime.currentProgress === anime.minProgress}
-                                onClick={() => updateAnimeProgress(anime, setUpdate)}>
+                                onClick={() => updateAnimeProgress(anime, setUpdate, setLoading)}>
                                     Watched
                                 </button>
                             </div>
                         </>
+                    }
+                    {loading && 
+                        (
+                            <div className='loading-container-update-anime'>
+                                <svg className='loading-update-anime' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8C121.8 95.6 64 169.1 64 256c0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1c-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256c0 141.4-114.6 256-256 256S0 397.4 0 256C0 140 77.1 42.1 182.9 10.6c16.9-5 34.8 4.6 39.8 21.5z"/>
+                                </svg>
+                                <p className='loading-update-anime-text'>Loading...</p>
+                            </div>
+                        )
                     }
                 </div>
             </div>
