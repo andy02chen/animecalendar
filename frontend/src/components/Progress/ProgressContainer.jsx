14e78e3de788b23d3aa7e96dd2d39082d89d21e4
Andy Chen <andy02chen@gmail.com>
2024-11-11 12:43:00 +1100

    Added styling to slide buttons

--- frontend/src/components/Progress/ProgressContainer.jsx
+++ frontend/src/components/Progress/ProgressContainer.jsx
@@ -197,7 +197,7 @@ function ProgressContainer({refresh, version}) {
                                 </button>
                                 <div id='progress-hidden-settings' style={{display: 'none'}} >
                                     <button className='progress-settings-buttons' onClick={() => expandStats()}>
-                                    <svg className='progress-settings-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M160 80c0-26.5 21.5-48 48-48l32 0c26.5 0 48 21.5 48 48l0 352c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48l0-352zM0 272c0-26.5 21.5-48 48-48l32 0c26.5 0 48 21.5 48 48l0 160c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48L0 272zM368 96l32 0c26.5 0 48 21.5 48 48l0 288c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48z"/></svg>
+                                        <svg className='progress-settings-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M160 80c0-26.5 21.5-48 48-48l32 0c26.5 0 48 21.5 48 48l0 352c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48l0-352zM0 272c0-26.5 21.5-48 48-48l32 0c26.5 0 48 21.5 48 48l0 160c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48L0 272zM368 96l32 0c26.5 0 48 21.5 48 48l0 288c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48z"/></svg>
                                         <p className='progress-settings-title'>&nbsp;Anime Stats</p>
                                     </button>
                                     <button className='progress-settings-buttons' onClick={() => expandNotice()}>
